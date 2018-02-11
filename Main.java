4e781730c85cf44ed135596acae0675afd89e4ee
Alvin Difuntorum <alvinpd09@gmail.com>
2018-02-12 00:02:09 +0100

    Update Main.java
    This is for conflict

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main{
     public static void main(String[] args) {
         Person alvin = new Person("Alvin", "Difuntorum");
         alvin.printName(); // Display Name
+        // I will cause conflict
     }
 }
