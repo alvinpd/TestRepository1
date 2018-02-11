45e43c903e3dcb0fe4b3737f39a5aa3ccbf5a761
Alvin Difuntorum <alvinpd09@gmail.com>
2018-02-11 23:53:33 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[] args) {
         Person alvin = new Person("Alvin", "Difuntorum");
-        alvin.printName();
+        alvin.printName(); // Display Name
     }
 }
